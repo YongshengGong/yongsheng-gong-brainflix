97965e2c056120eb7fe24b42898f34a81d1a1b8c
Yongsheng <ygong56@uwo.ca>
2024-06-01 11:40:05 -0400

    I added a upload button and a logo at the top

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,12 @@ import "./Header.scss";
 function Header(){
     return (
     <header className="header">
-       <section className="header__logo-title-container">
-           <span>BrainFlix</span>
-       </section>
+       <section className="header__logo-title-container"></section>
        <section className="header__search-avatar-container">
            <input className="header__search" placeholder="Search"></input>
            <div className="header__avatar"></div>
        </section>
+       <button className="header__upload-button">UPLOAD</button>
     </header>
     )
 }
