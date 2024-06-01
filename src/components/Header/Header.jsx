ee4fa2dd6b131b30320e92693e5e28f0e58e30b8
Yongsheng <ygong56@uwo.ca>
2024-05-31 23:56:49 -0400

    I created a Header function in Header.jsx file and included header element, I also started writting some styling codes in Header.scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
-import "./Header.css";
+import "./Header.scss";
+import avata from "../../assets/images/Mohan-muruge.jpg";
 function Header(){
-    return 
-    <>
-    </>
+    return (
+    <header className="header">
+       <section className="header__logo-title-container">
+           <span>BrainFlix</span>
+       </section>
+       <section className="header__search-avatar-container">
+           <input className="header__search" placeholder="Search"></input>
+           <img className="header__avatar" src={avata} alt="" />
+       </section>
+    </header>
+    )
 }
 export default Header;
