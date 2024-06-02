fd1c5c663fed2b465992777085ababefad062eb8
Yongsheng <ygong56@uwo.ca>
2024-06-02 01:37:44 -0400

    I converted all timestamps in milliseconds to month/day/year format

--- src/components/VideoDetails/CommentsDisplay/CommentsDisplay.jsx
+++ src/components/VideoDetails/CommentsDisplay/CommentsDisplay.jsx
@@ -5,12 +5,13 @@ function CommentsDisplay({ video }) {
             {
                 video.comments.map(
                     comment => {
+                        const time=new Date(comment.timestamp).toLocaleDateString("en-us");
                         return <article className="video-details__container-comments-display-comment">
                                   <span className="video-details__container-comments-display-comment-left"></span>
                                   <section className="video-details__container-comments-display-comment-right">
                                     <section className="video-details__container-comments-display-comment-right-top">
                                        <span className="video-details__container-comments-display-comment-right-top-name">{comment.name}</span>
-                                       <span className="video-details__container-comments-display-comment-right-top-time">{comment.timestamp}</span>
+                                       <span className="video-details__container-comments-display-comment-right-top-time">{time}</span>
                                     </section>   
                                     <p className="video-details__container-comments-display-comment-right-bottom">{comment.comment}</p>
                                   </section>
--- src/components/VideoDetails/SubSection/SubSection.jsx
+++ src/components/VideoDetails/SubSection/SubSection.jsx
@@ -1,10 +1,11 @@
 import "./SubSection.scss";
 function SubSection({ video }) {
+  const time=new Date(video.timestamp).toLocaleDateString("en-us");
     return (
         <section className="video-details__container-subSection">
             <span className="video-details__container-subSection-channel">By {video.channel}</span>
             <span className="video-details__container-subSection-views">{video.views}</span>
-            <span className="video-details__container-subSection-time">{video.timestamp}</span>
+            <span className="video-details__container-subSection-time">{time}</span>
             <span className="video-details__container-subSection-likes">{video.likes}</span>
         </section>
     )
