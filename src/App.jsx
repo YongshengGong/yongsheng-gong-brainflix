d8c4d9d711c1f5419bf0f34516fc4b0378fc9036
Yongsheng <ygong56@uwo.ca>
2024-06-01 21:25:11 -0400

    I changed the App extention name from css to scss and now imported the resets partial in App.scss instead of in originally Header.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 import Header from "./components/Header/Header";
 import VideoDetails from './components/VideoDetails/VideoDetails';
 import videos from "./data/video-details.json";
