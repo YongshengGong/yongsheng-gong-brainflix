31dfc59cb8baabd480ccfbb4f17eab7a166ed771
Yongsheng <ygong56@uwo.ca>
2024-06-01 20:18:08 -0400

    I created a basicand plain structure for the video-details section without any styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Header from "./components/Header/Header";
 import VideoDetails from './components/VideoDetails/VideoDetails';
+import videos from "./data/video-details.json";
 function App() {
+     const [selectedVideo,setSelectedVideo]=useState(videos[0]);
   return (
     <>
        <Header/>
        <main>
-         <VideoDetails/>
+         <VideoDetails video={selectedVideo}/>
        </main>
     </>
   )
--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -1,8 +1,48 @@
-function VideoDetails(){
-return(
-    <section className="video-details">
-
-    </section>
-)
+import React from "react";
+import "./VideoDetails.scss";
+function VideoDetails({video}) {
+    return (
+        <section className="video-details">
+             <video className="video-details__play" poster={video.image} controls></video>
+             <h1 className="video-details__title">{video.title}</h1>
+             <section className="video-details__subSection">
+                <span className="video-details__subSection-channel">{video.channel}</span>
+                <span className="video-details__subSection-views">{video.views}</span>
+                <span className="video-details__subSection-time">{video.timestamp}</span>
+                <span className="video-details__subSection-likes">{video.timestamp}</span>
+                <span  >{video.likes}</span>
+             </section>
+             <p className="video-details__description">
+                {video.description}
+             </p>
+             <section className="video-details__comments">
+                 <h3 className="video-details__comments-title">{video.comments.length} {video.comments.length===1?("Comment"):("Comments")}</h3>
+                 <section className="video-details__comments-input">
+                    <span className="video-details__comments-input-avatar"></span>
+                    <form className="video-details__comments-input-form">
+                        <label className="video-details__comments-input-form-title">JOIN THE CONVERSATION</label>
+                        <textarea className="video-details__comments-input-form-field"></textarea>
+                        <button className="video-details__comments-input-form-button">COMMENT</button>
+                    </form>
+                 </section>
+                 <section className="video-details__comments-display">
+                     {
+                        video.comments.map(
+                            comment=>{
+                               return <article className="video-details__comments-display-comment">
+                                          <span className="video-details__comments-display-comment-left"></span>
+                                          <section className="video-details__comments-display-comment-right">
+                                               <span className="video-details__comments-display-comment-right-name">{comment.name}</span>
+                                               <span className="video-details__comments-display-comment-right-time">{comment.timestamp}</span>
+                                               <p className="video-details__comments-display-comment-right-content">{comment.comment}</p>
+                                          </section>
+                                      </article>
+                            }
+                        )
+                     }
+                 </section>
+             </section>
+        </section>
+    )
 }
 export default VideoDetails;
