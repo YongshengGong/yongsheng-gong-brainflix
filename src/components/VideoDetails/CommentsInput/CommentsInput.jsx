6d1ffdd63f480dc4dad023d6a834a094c658ff4d
Yongsheng <ygong56@uwo.ca>
2024-06-02 00:27:33 -0400

    I added styling for class video-details__container-comments-input in CommentsInput.scss

--- src/components/VideoDetails/CommentsInput/CommentsInput.jsx
+++ src/components/VideoDetails/CommentsInput/CommentsInput.jsx
@@ -1,10 +1,11 @@
+import "./CommentsInput.scss"
 function CommentsInput() {
     return (
         <section className="video-details__container-comments-input">
             <span className="video-details__container-comments-input-avatar"></span>
             <form className="video-details__container-comments-input-form">
                 <label className="video-details__container-comments-input-form-title">JOIN THE CONVERSATION</label>
-                <textarea className="video-details__container-comments-input-form-field"></textarea>
+                <textarea className="video-details__container-comments-input-form-field" placeholder="Add a new comment"></textarea>
                 <button className="video-details__container-comments-input-form-button">COMMENT</button>
             </form>
         </section>
