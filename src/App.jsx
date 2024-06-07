f0c754717710b4890a9acb848df3b5261238c24e
Yongsheng <ygong56@uwo.ca>
2024-06-06 21:59:09 -0400

    add useEffect to call an api function to fetch data from the endpoint /video

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.scss'
 import Header from "./components/Header/Header";
 import VideoDetails from './components/VideoDetails/VideoDetails';
 import videos from "./data/video-details.json";
 import NextVideos from './components/NextVideos/NextVideos';
 import Player from './components/Player/Player';
+import axios from 'axios';
 function App() {
+  const url="https://unit-3-project-api-0a5620414506.herokuapp.com/";
+  const api_key="3e1b084f-72fa-4a65-8938-76c9f8b3a923";
+  useEffect(()=>{
+   async function callApi(){
+      const res = await axios.get(`${url}videos?api_key=${api_key}`);
+      console.log(res.data);
+   }
+   callApi();
+  },[])
   const [selectedVideo, setSelectedVideo] = useState(videos[0]);
   const [list,setList]=useState(videos);
   function check(id) {
