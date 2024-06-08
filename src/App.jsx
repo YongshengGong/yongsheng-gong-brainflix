a1c3c03efdf9091f7da3e9302a604ac9dde7f4dc
Yongsheng <ygong56@uwo.ca>
2024-06-07 20:40:27 -0400

    Rename App component to HomePage component and put inside pages folder and create a new App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,13 @@
+import "./App.scss"
 import React, { useEffect, useState } from 'react'
-import './App.scss'
-import Header from "./components/Header/Header";
-import VideoDetails from './components/VideoDetails/VideoDetails';
-// import videos from "./data/video-details.json";
-import NextVideos from './components/NextVideos/NextVideos';
-import Player from './components/Player/Player';
-import axios from 'axios';
-function App() {
-  const url="https://unit-3-project-api-0a5620414506.herokuapp.com/";
-  const api_key="3e1b084f-72fa-4a65-8938-76c9f8b3a923";
-  const [selectedVideo, setSelectedVideo] = useState(null);
-  const [list,setList]=useState([]);
-  const [loading,setLoading]=useState(true);
+import HomePage from "./pages/HomePage"
 
-  useEffect(()=>{
-   async function callApi(){
-    let completeList=[];
-   try{
-      const res = await axios.get(`${url}videos?api_key=${api_key}`);
-      for (const obj of res.data) {
-        const videoDetails = await callApiCompleteList(obj);
-        completeList.push(videoDetails.data);
-      }
-      setSelectedVideo(completeList[0]);
-      setList([...list,...completeList]);
-   }
-   catch(error){
-    console.log(error);
-   }
-  setLoading(false);
-   }
-   callApi();
-  },[]);
-  
-
-  async function callApiCompleteList(obj){
-    try{
-      const res = await axios.get(`${url}videos/${obj.id}?api_key=${api_key}`);
-      return res;
-    }
-    catch(error){
-        console.log(error)
-    }
-  }
-
-
-  function check(id) {
-    const filter = list.find(obj => obj.id === id);
-    setSelectedVideo(filter);
-  }
-  const newList=list.filter(obj=>obj.id!==selectedVideo.id);
-   console.log(list)
-  if(loading){
-    return <h1>Loading...</h1>
-  }
-  else{
-  return (
-    <>
-      <Header />
-      <Player video={selectedVideo}/>
-      <main className="main">
-        <VideoDetails video={selectedVideo} />
-        <NextVideos list={newList} check={check} />
-      </main>
-    </>
-  )
-}
+function App(){
+    return(
+        <>
+          <HomePage></HomePage>
+        </>
+    )
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import './HomePage.scss'
+import Header from "../components/Header/Header";
+import VideoDetails from '../components/VideoDetails/VideoDetails';
+// import videos from "./data/video-details.json";
+import NextVideos from '../components/NextVideos/NextVideos';
+import Player from '../components/Player/Player';
+import axios from 'axios';
+function HomePage() {
+  const url="https://unit-3-project-api-0a5620414506.herokuapp.com/";
+  const api_key="3e1b084f-72fa-4a65-8938-76c9f8b3a923";
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [list,setList]=useState([]);
+  const [loading,setLoading]=useState(true);
+
+  useEffect(()=>{
+   async function callApi(){
+    const completeList=[];
+   try{
+      const incompleteList = await axios.get(`${url}videos?api_key=${api_key}`);
+      for (const incompleteListObject of incompleteList.data) {
+        const completeListObject = await callApiCompleteList(incompleteListObject);
+        completeList.push(completeListObject.data);
+      }
+      setSelectedVideo(completeList[0]);
+      setList([...completeList]);
+   }
+   catch(error){
+    console.log(error);
+   }
+  setLoading(false);
+   }
+   callApi();
+  },[]);
+  
+
+  async function callApiCompleteList(obj){
+    try{
+      const res = await axios.get(`${url}videos/${obj.id}?api_key=${api_key}`);
+      return res;
+    }
+    catch(error){
+        console.log(error)
+    }
+  }
+
+
+  function check(id) {
+    const filter = list.find(obj => obj.id === id);
+    setSelectedVideo(filter);
+  }
+  const newList=list.filter(obj=>obj.id!==selectedVideo.id);
+   console.log(list)
+  if(loading){
+    return <h1>Loading...</h1>
+  }
+  else{
+  return (
+    <>
+      <Header />
+      <Player video={selectedVideo}/>
+      <main className="main">
+        <VideoDetails video={selectedVideo} />
+        <NextVideos list={newList} check={check} />
+      </main>
+    </>
+  )
+}
+}
+
+export default HomePage
