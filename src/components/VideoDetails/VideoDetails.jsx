ddae46c9a1827f4e058d9507fe034e1dcffb6c3b
Yongsheng <ygong56@uwo.ca>
2024-06-07 21:45:32 -0400

    Improvement from brainflix sprint1: make sure all elements adhere to BEM convention in VideoDetails component

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -6,13 +6,13 @@ import CommentsDisplay from "./CommentsDisplay/CommentsDisplay";
 function VideoDetails({ video }) {
     return (
             <section className="video-details">
-                <h1 className="video-details-title">{video.title}</h1>
+                <h1 className="video-details__title">{video.title}</h1>
                 <SubSection video={video} />
-                <p className="video-details-description">
+                <p className="video-details__description">
                     {video.description}
                 </p>
-                <section className="video-details-comments">
-                    <h3 className="video-details-comments-title">
+                <section className="video-details__comments">
+                    <h3 className="video-details__comments-title">
                         {video.comments.length}
                         {" "}
                         {video.comments.length === 1 ? ("Comment") : ("Comments")}
