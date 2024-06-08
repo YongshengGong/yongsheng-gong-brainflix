83f5589a309132f525a1735ad75871c44267efb1
Yongsheng <ygong56@uwo.ca>
2024-06-07 22:38:11 -0400

    Improve BEM naming for SubSection component; create another wrapper container inside SubSection component to make it easier to set padding on the container while leaving white space beside children's border

--- src/components/VideoDetails/SubSection/SubSection.jsx
+++ src/components/VideoDetails/SubSection/SubSection.jsx
@@ -2,15 +2,17 @@ import "./SubSection.scss";
 function SubSection({ video }) {
     const time = new Date(video.timestamp).toLocaleDateString("en-us");
     return (
-        <section className="video-details__container-subSection">
-            <section className="video-details__container-subSection-container1">
-                <span className="video-details__container-subSection-container1-channel">By {video.channel}</span>
-                <span className="video-details__container-subSection-container1-time">{time}</span>
-            </section>
+        <section className="video-details__subSection">
+            <section className="video-details__subSection-container">
+                <section className="video-details__subSection-container-container1">
+                    <span className="video-details__subSection-container-container1-channel">By {video.channel}</span>
+                    <span className="video-details__subSection-container-container1-time">{time}</span>
+                </section>
 
-            <section className="video-details__container-subSection-container2">
-                <span className="video-details__container-subSection-container2-views">{video.views}</span>
-                <span className="video-details__container-subSection-container2-likes">{video.likes}</span>
+                <section className="video-details__subSection-container-container2">
+                    <span className="video-details__subSection-container-container2-views">{video.views}</span>
+                    <span className="video-details__subSection-container-container2-likes">{video.likes}</span>
+                </section>
             </section>
         </section>
     )
