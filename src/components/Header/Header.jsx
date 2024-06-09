fc9655547b06e0fcaa94ebfe329c91f58e01768e
Yongsheng <ygong56@uwo.ca>
2024-06-09 00:21:10 -0400

    Add a click handler to the upload button; also add divider line for desktop screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,10 @@
 import "./Header.scss";
+import { useNavigate } from "react-router-dom";
 function Header() {
+    const nav=useNavigate();
+    function handleClick(){
+      nav("/upload");
+    }
     return (
         <header className="header">
             <section className="header__logo"></section>
@@ -7,7 +12,9 @@ function Header() {
                 <input className="header__search" placeholder="Search"></input>
                 <span className="header__avatar header__avatar-mobile"></span>
             </section>
-            <button className="header__upload-button">UPLOAD</button>
+            <button className="header__upload-button"
+              onClick={handleClick}
+            >UPLOAD</button>
             <span className="header__avatar"></span>
         </header>
     )
