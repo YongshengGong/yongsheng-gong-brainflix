9e4991e8500610e596155bd45985b192545568c2
Yongsheng <ygong56@uwo.ca>
2024-06-09 13:32:41 -0400

    Add a clickHandler to logo to route to the homePage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,21 @@ import "./Header.scss";
 import { useNavigate } from "react-router-dom";
 function Header() {
     const nav=useNavigate();
-    function handleClick(){
+    function handleUploadClick(){
       nav("/upload");
+    }
+    function handleLogoClick(){
+      nav("/");
     }
     return (
         <header className="header">
-            <section className="header__logo"></section>
+            <section className="header__logo" onClick={handleLogoClick}></section>
             <section className="header__search-avatar-container">
                 <input className="header__search" placeholder="Search"></input>
                 <span className="header__avatar header__avatar-mobile"></span>
             </section>
             <button className="header__upload-button"
-              onClick={handleClick}
+              onClick={handleUploadClick}
             >UPLOAD</button>
             <span className="header__avatar"></span>
         </header>
