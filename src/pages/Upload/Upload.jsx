0f3f6acdce7c6015d973f05fc59da282c0d65a60
Yongsheng <ygong56@uwo.ca>
2024-06-09 00:51:07 -0400

    Add html structure to the Upload component

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,10 +1,28 @@
 import "./Upload.scss"
 import Header from "../../components/Header/Header";
-function Upload(){
+function Upload() {
     return (
         <>
-        <Header></Header>
-        upload page
+            <Header></Header>
+            <main className="upload">
+                <span className="upload__title">Upload Video</span>
+                <section className="upload__thumbnail">
+                    <span className="upload__thumbnail-title">VIDEO THUMBNAIL</span>
+                    <img src="" alt="" className="upload__thumbnail-content"/>
+                </section>
+                <form className="upload__form">
+                    <label className="upload__form-label1">
+                        <span>TITLE YOUR VIDEO</span>
+                        <input type="text" />
+                    </label>
+                    <label className="upload__form-label2">
+                        <span>ADD A VIDEO DESCRIPTION</span>
+                        <textarea type="text" />
+                    </label>
+                    <button className="upload__form-button-publish">PUBLISH</button>
+                    <button className="upload__form-button-cancel">CANCEL</button>
+                </form>
+            </main>
         </>
     )
 }
