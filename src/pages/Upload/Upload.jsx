d02d4be9ed06958821172ea8cd62f15994bed506
Yongsheng <ygong56@uwo.ca>
2024-06-09 02:39:25 -0400

    Finish with Upload component styling for mobile screen

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,5 +1,6 @@
 import "./Upload.scss"
 import Header from "../../components/Header/Header";
+import uploadImage from "../../assets/images/Upload-video-preview.jpg";
 function Upload() {
     return (
         <>
@@ -8,16 +9,16 @@ function Upload() {
                 <span className="upload__title">Upload Video</span>
                 <section className="upload__thumbnail">
                     <span className="upload__thumbnail-title">VIDEO THUMBNAIL</span>
-                    <img src="" alt="" className="upload__thumbnail-content"/>
+                    <img src={uploadImage} alt="" className="upload__thumbnail-content"/>
                 </section>
                 <form className="upload__form">
                     <label className="upload__form-label1">
                         <span>TITLE YOUR VIDEO</span>
-                        <input type="text" />
+                        <input type="text" placeholder="Add a title to your video"/>
                     </label>
                     <label className="upload__form-label2">
                         <span>ADD A VIDEO DESCRIPTION</span>
-                        <textarea type="text" />
+                        <textarea type="text" placeholder="Add a description to your video"/>
                     </label>
                     <button className="upload__form-button-publish">PUBLISH</button>
                     <button className="upload__form-button-cancel">CANCEL</button>
