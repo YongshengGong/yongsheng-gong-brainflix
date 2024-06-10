d41532ca969e6faca0f3836298a738223a11c226
Yongsheng <ygong56@uwo.ca>
2024-06-09 23:36:57 -0400

    Add a key property to the returned element created by map() method for CommentsDisplay component

--- src/components/VideoDetails/CommentsDisplay/CommentsDisplay.jsx
+++ src/components/VideoDetails/CommentsDisplay/CommentsDisplay.jsx
@@ -8,7 +8,8 @@ function CommentsDisplay({ video }) {
                         const time = new Date(comment.timestamp).toLocaleDateString("en-us");
                         return <article className={index === comments.length - 1 ?
                             "video-details__text-comments-display-comment video-details__text-comments-display-comment--last" :
-                            "video-details__text-comments-display-comment"}>
+                            "video-details__text-comments-display-comment"}
+                            key={comment.id}>
                             <span className="video-details__text-comments-display-comment-left"></span>
                             <section className="video-details__text-comments-display-comment-right">
                                 <section className="video-details__text-comments-display-comment-right-top">
